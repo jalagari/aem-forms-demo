cd7d318f965031bacc3e9bb83f38fcc555b28e9c
Vijay Kumar Jalagari <jalagari@adobe.com>
2026-04-20 22:47:09 +0530

    Form Id should be used in action as 1st prioirty

--- blocks/form/form.js
+++ blocks/form/form.js
@@ -537,7 +537,7 @@ export default async function decorate(block) {
       // Check if we're in an iframe and use parent window path if available
       const iframePath = window.frameElement ? window.parent.location.pathname
         : window.location.pathname;
-      formDef.action = SUBMISSION_SERVICE + btoa(pathname || iframePath);
+      formDef.action = SUBMISSION_SERVICE + (formDef.id ? formDef.id : (btoa(pathname || iframePath)));
     } else {
       formDef.action = getSubmitBaseUrl() + (formDef.action || '');
     }
